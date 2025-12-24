a54fbef2e6be4b3ec5fd9d721b27157fbd86c241
Luong Nguyen Viet Sang <23020762@vnu.edu.vn>
2025-12-24 21:35:10 +0700

    final_final

--- apps/api/src/config/cors.js
+++ apps/api/src/config/cors.js
@@ -4,7 +4,16 @@
  */
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
+  origin: function (origin, callback) {
+    const isVercelPreview = origin && origin.endsWith('.vercel.app');
+    const frontendUrl = (process.env.FRONTEND_URL).replace(/\/$/, "");
+
+    if (!origin || origin.replace(/\/$/, "") === frontendUrl || isVercelPreview) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
